d932f8cc8f730d2336a9439440014b573aea98ce
Rodrigo Soares <diihhsoares2@gmail.com>
2023-04-26 17:40:54 -0300

    criando componente  cotacoes moedas

---
+++ src/components/CotacoesMoedas.vue
@@ -0,0 +1,41 @@
+<template>
+  <table class="table">
+    <thead>
+        <tr>
+              <th>Código</th>
+              <th>Nome</th>
+              <th>Máximo</th>
+              <th>Mínimo</th>
+              <th>Variação</th>
+              <th></th>
+        </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(quote, key) in quotes" :key="key">
+        <td> {{ key }}</td>
+        <td> {{quote.name }}</td>
+        <td>{{quote.high }}</td>
+        <td> {{ quote.low }}</td>
+        <td>
+          <span
+           class="label label-rounded text-small" 
+          :class="{'label-error': quote.pctChange < 0, 'label-success': quote.pctChange > 0 }">              
+            {{ quote.pctChange }} %
+          </span>
+        </td>    
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+     props: { quotes: { type: Object, required: true }},
+  }
+</script>
+
+<style>
+  th, td {
+    font-size: 20px;
+  }
+</style>
