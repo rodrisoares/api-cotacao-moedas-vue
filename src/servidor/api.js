f56ffef478d8c3ac8e5acd2b01ebe4c0248b669b
Rodrigo Soares <diihhsoares2@gmail.com>
2023-04-26 17:41:14 -0300

    criando conexao com a api

---
+++ src/servidor/api.js
@@ -0,0 +1,12 @@
+// Configurações da API
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://economia.awesomeapi.com.br/json',
+});
+
+export default {
+  all() {
+    return api.get('/all')
+  }
+}
